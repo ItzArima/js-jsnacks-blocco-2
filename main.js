5a341bc68e74e63b8008fafe9e629406205655d0
ItzArima <skaypaprilia@gmail.com>
2021-10-25 11:32:52 +0200

    creata pagina index e array bicidacorsa

---
+++ main.js
@@ -0,0 +1,16 @@
+const bicidacorsa = [
+    {
+        nome : 'bici1',
+        peso : 0.9
+    },
+    {
+        nome : 'bici2',
+        peso : 7.0 
+    },
+    {
+        nome : 'bici3',
+        peso : 3.0
+    }
+]
+
+console.log(bicidacorsa[0].peso);
